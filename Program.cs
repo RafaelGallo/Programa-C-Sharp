41e9318104c315911d788fa0a09d22361a5e0238
Rafael Gallo <44452165+RafaelGallo@users.noreply.github.com>
2019-03-01 00:29:04 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto__Hello_World_em_linguas_
+{
+	static class Program
+	{
+		/// <summary>
+		/// Ponto de entrada principal para o aplicativo.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
+}
